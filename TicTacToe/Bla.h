23e5725ab492911d7dda14e775ebc86c5b4f0757
Markus Rührmair <jean10marc@Markuss-MacBook-Pro.local>
2012-12-30 15:41:17 +0100

    Project File

---
+++ TicTacToe/Bla.h
@@ -0,0 +1,13 @@
+//
+//  Bla.h
+//  TicTacToe
+//
+//  Created by Markus Rührmair on 28.12.12.
+//  Copyright (c) 2012 shiftingbytes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Bla : NSObject
+
+@end
---
+++ TicTacToe/Bla.m
@@ -0,0 +1,13 @@
+//
+//  Bla.m
+//  TicTacToe
+//
+//  Created by Markus Rührmair on 28.12.12.
+//  Copyright (c) 2012 shiftingbytes. All rights reserved.
+//
+
+#import "Bla.h"
+
+@implementation Bla
+
+@end
